e996442173a3bf33441ce2f2c7dc8b05a5c7d5c3
Christian Popescu <christian.popescu@outlook.com>
2020-05-25 23:33:27 +0200

    Modify position as Class

--- src/Sandbox/SanboxConsole/src/Position.cs
+++ src/Sandbox/SanboxConsole/src/Position.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace SanboxConsole
 {
-    public struct Position
+    public class Position
     {
 
         public Position(int r, int c)
--- src/Sandbox/SanboxConsole/src/Text.cs
+++ src/Sandbox/SanboxConsole/src/Text.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 
 namespace SanboxConsole
@@ -6,14 +7,33 @@ namespace SanboxConsole
     {
         public List<string> content = new List<string>();
 
+        private Position _max;
+
         public Text()
         {
-            
+
 
         }
         public Text(List<string> content)
         {
             this.content = content;
+        }
+
+        public Position Max 
+        {
+            get 
+            {
+                if (_max == null)
+                {
+                    _max = new Position(0,0);
+                    _max.raw = content.Count;
+                    _max.column = 0;
+                    foreach (var s in content)
+                        if (_max.column < s.Length) _max.column = s.Length;
+                }
+                return _max;
+
+            }
         }
     }
 }
