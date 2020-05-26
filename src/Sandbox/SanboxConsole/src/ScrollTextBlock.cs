e6a073a5a23bc233e8a2d4171462d05fd1e5eb6b
Christian Popescu <christian.popescu@outlook.com>
2020-05-26 23:43:55 +0200

    Modify ScrollTextBlock add size and top Left corner

--- src/Sandbox/SanboxConsole/src/ScrollTextBlock.cs
+++ src/Sandbox/SanboxConsole/src/ScrollTextBlock.cs
@@ -4,6 +4,12 @@ namespace SanboxConsole
     {
         public Text text;
 
+        public Position size;
+
+        public Position leftTopCorner = new Position(0,0);
+
+        public ScrollTextBlock(Position size) => this.size = size;
+
         public ScrollTextBlock(Text text) => this.text = text;
 
         public ScrollTextBlock()
